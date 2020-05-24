1d531174d676c7132a0e2c18094b77e89ac9b0a3
John Siu <john.sd.siu@gmail.com>
2020-05-24 18:30:28 -0400

    - detect and use host audio GID from /dev/snd/timer - run mpd as normal user

--- docker/mpd_lite/start.sh
+++ docker/mpd_lite/start.sh
@@ -22,9 +22,16 @@ fi
 deluser ${PUSR}
 delgroup ${PUSR}
 
+# detect and use host audio GID from /dev/snd/timer
+AGID=$(stat -c %g /dev/snd/timer)
+delgroup audio
+addgroup -g ${AGID} audio
+
 addgroup -g ${PGID} ${PUSR}
 adduser -D -h ${PHOME} -G ${PUSR} -u ${PUID} ${PUSR}
-adduser mpd audio
+adduser ${PUSR} audio
 chown ${PUSR}:${PUSR} ${PHOME}
 
-${PROG} --stdout --no-daemon /mpd.conf
+su - mpd -c "${PROG} \"--no-daemon\" \"/mpd.conf\""
+# Debug
+#su - mpd -c "${PROG} \"--stdout\" \"--no-daemon\" \"/mpd.conf\""
